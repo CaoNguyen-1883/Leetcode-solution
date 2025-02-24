9d50726d2087c6bca773cf5ea381ae66ab7e113f
CaoNguyen-1883 <nguyendadenkrb@gmail.com>
2025-02-24 12:40:36 +0700

    12:40 2/24/2025

---
+++ 475. Heaters.cpp
@@ -0,0 +1,21 @@
+#include"LeetCodeLibs.h"
+
+class Solution {
+public:
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        sort(houses.begin(), houses.end());
+        sort(heaters.begin(), heaters.end());
+        
+        int ans = 0;
+        int i = 0; 
+
+        for (const int house : houses) {
+            while (i + 1 < heaters.size() &&
+                    house - heaters[i] > heaters[i + 1] - house)
+                ++i; 
+            ans = max(ans, abs(heaters[i] - house));
+        }
+
+        return ans;
+    }
+}
