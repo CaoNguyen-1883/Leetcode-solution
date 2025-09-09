1a7ec804dc5b31cf5fc6ec2a3e3a099bcb12aa11
CaoNguyen-1883 <nguyendadenkrb@gmail.com>
2025-09-09 09:20:28 +0700

    _

--- 22. Generate Parentheses.java
+++ 22. Generate Parentheses.java
@@ -1,23 +1,33 @@
-import java.util.ArrayList;
+'import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
         
-        solve(res, new StringBuilder(), n*2);
+        solve(res, new StringBuilder(), n, n);
     
         return res;
     }
 
-    public void solve(List<String> res, char[] str, int i, int n){
-        if(sb.length() == n){
+    public void solve(List<String> res, StringBuilder sb, int l, int r){
+        if(l == 0 && r == 0){
             res.add(sb.toString());
             return;
         }
 
-        sb.append('(');
-        solve(res, sb, n);
-        sb.deleteCharAt(sb.length() - 1);
+        if(l > 0){
+            sb.append('(');
+            solve(res, sb, l - 1, r);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        if(l < r){
+            sb.append(')');
+            solve(res, sb, l, r - 1);
+            sb.deleteCharAt(sb.length() - 1);
+        }
     }
+
 }
+
+//,,, ()
--- 23. Merge k Sorted Lists.java
+++ 23. Merge k Sorted Lists.java
@@ -1,49 +1,32 @@
 import java.util.PriorityQueue;
+import java.util.Queue;
 
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
-class Pair {
-    int val;
-    ListNode listNode;
 
-    Pair(int val, ListNode listNode) {
-        this.val = val;
-        this.listNode = listNode;
-    }
-}
 
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<Pair> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
+        ListNode head = new ListNode(0);
+        ListNode node = head;
 
-        for (ListNode head : lists) {
-            if (head != null) {
-                minHeap.offer(new Pair(head.val, head));
+
+        Queue <Pair> q = new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        for(ListNode i : lists){
+            if(i != null){
+                q.add(new Pair(i.val, i));
             }
         }
 
-        if (minHeap.isEmpty()) return null;
-
-        ListNode res = new ListNode(0);
-        ListNode node = res;
-
-        while (!minHeap.isEmpty()) {
-            Pair p = minHeap.poll();
-
-            node.next = p.listNode;
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            node.next = new ListNode(p.val);
             node = node.next;
-
-            if (p.listNode.next != null) {
-                minHeap.offer(new Pair(p.listNode.next.val, p.listNode.next));
+            if(p.listNode.next != null){
+                q.add(new Pair(p.listNode.next.val, p.listNode.next));
             }
         }
 
-        return res.next;
+        return head.next;
     }
 
     static class Pair {
@@ -56,3 +39,43 @@ class Solution {
         }
     }
 } 
+
+// class Solution {
+//     public ListNode mergeKLists(ListNode[] lists) {
+//         PriorityQueue<Pair> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
+
+//         for (ListNode head : lists) {
+//             if (head != null) {
+//                 minHeap.offer(new Pair(head.val, head));
+//             }
+//         }
+
+//         if (minHeap.isEmpty()) return null;
+
+//         ListNode res = new ListNode(0);
+//         ListNode node = res;
+
+//         while (!minHeap.isEmpty()) {
+//             Pair p = minHeap.poll();
+
+//             node.next = p.listNode;
+//             node = node.next;
+
+//             if (p.listNode.next != null) {
+//                 minHeap.offer(new Pair(p.listNode.next.val, p.listNode.next));
+//             }
+//         }
+
+//         return res.next;
+//     }
+
+//     static class Pair {
+//         int val;
+//         ListNode listNode;
+
+//         Pair(int val, ListNode listNode) {
+//             this.val = val;
+//             this.listNode = listNode;
+//         }
+//     }
+// } 
