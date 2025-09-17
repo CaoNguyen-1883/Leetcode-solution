79523bb55d832c6695528d56d607377d35952843
CaoNguyen-1883 <nguyendadenkrb@gmail.com>
2025-09-17 13:53:57 +0700

    .

--- 33. Search in Rotated Sorted Array.java
+++ 33. Search in Rotated Sorted Array.java
@@ -2,26 +2,20 @@ class Solution {
     public int search(int[] nums, int target) {
         int l = 0;
         int r = nums.length - 1;
-
-        while (l <= r) {
-        final int m = (l + r) / 2;
-        if (nums[m] == target)
-            return m;
-        if (nums[l] <= nums[m]) { // nums[l..m] are sorted.
-            if (nums[l] <= target && target < nums[m])
-                r = m - 1;
-            else
-                l = m + 1;
-        } else { // nums[m..n - 1] are sorted.
-            if (nums[m] < target && target <= nums[r])
-                l = m + 1;
-            else
-                r = m - 1;
-        }
+        while(l <= r){
+            int m = (l + r) / 2;
+            if(nums[m] == target) return m;
+            else if(nums[l] <= nums[m]){
+                if(nums[l] <= target && target < nums[m]) r = m - 1;
+                else l = m + 1;
+            } else{
+                if(target > nums[m] && target <= nums[r]) l = m + 1;
+                else r = m - 1;
+            }
         }
 
         return -1;
     }
 }
 
-/// 4 5 6 1 2 3
+/// 4 5 5 5 6 1 2 3
---
+++ 34. Find First and Last Position of Element in Sorted Array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        if(nums.length == 0) return new int[]{-1, -1};
+    
+        int l = 0;
+        int r = nums.length - 1;
+        while(l < r){
+            int m = (l + r) / 2;
+            if(target <= nums[m]) r = m - 1;
+            else l = m + 1; 
+        }
+
+        if(l == nums.length || nums[l] != target) return new int[]{-1, -1};
+
+        int i = l;
+
+        
+        r = nums.length - 1;
+
+        while(l <= r){
+            int m = (l + r) / 2;
+            if(target >= nums[m]) l = m + 1;
+            else r = m - 1;
+        }
+
+
+        return new int[]{i, r};
+    }
+}
